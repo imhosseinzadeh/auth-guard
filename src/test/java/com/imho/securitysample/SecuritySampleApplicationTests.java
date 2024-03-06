76aec685120edaa33fc011b73d971c27a832d94e
imhosseinzadeh <imhosseinzadeh@gmail.com>
2024-03-06 15:55:08 +0330

    initial project

---
+++ src/test/java/com/imho/securitysample/SecuritySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imho.securitysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
