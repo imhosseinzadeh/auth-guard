d5d6921812acbec754153d195b4e5c13c2fc9ff5
imhosseinzadeh <imhosseinzadeh@gmail.com>
2025-08-13 19:19:14 +0330

    feat: add solution property to error response

--- src/main/java/com/imho/authguard/exception/GlobalExceptionHandler.java
+++ src/main/java/com/imho/authguard/exception/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.imho.authguard.exception;
 
 import com.imho.authguard.exception.domain.DomainException;
-import com.imho.authguard.exception.domain.notfound.NotFoundException;
 import com.imho.authguard.exception.domain.confilict.ConflictException;
 import com.imho.authguard.exception.domain.expired.ExpiredException;
+import com.imho.authguard.exception.domain.notfound.NotFoundException;
 import com.imho.authguard.exception.infrastructure.InfrastructureException;
 import com.imho.authguard.infra.i18n.MessageResolver;
 import lombok.RequiredArgsConstructor;
@@ -35,19 +35,11 @@ public class GlobalExceptionHandler {
 
         HttpStatus status = resolveHttpStatus(ex);
         ProblemDetail problem = buildProblemDetail(status, ex.getTitle(), ex.getMessage());
+        problem.setProperty("solution", ex.getSolution());
 
         return ResponseEntity
                 .status(status)
                 .body(problem);
-    }
-
-    @ExceptionHandler(InfrastructureException.class)
-    public ResponseEntity<ProblemDetail> handleInfrastructureException(InfrastructureException ex) {
-        log.error("Handled InfrastructureException: {}", ex.getMessage(), ex);
-
-        return ResponseEntity
-                .status(ex.getStatusCode())
-                .body(ex.getBody());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -56,6 +48,7 @@ public class GlobalExceptionHandler {
         String detail = messageResolver.getMessage("error.validation.detail");
 
         ProblemDetail problem = buildProblemDetail(HttpStatus.BAD_REQUEST, title, detail);
+        problem.setProperty("solution", "error.validation.solution");
 
         // Set validation errors
         List<Map<String, String>> errors = ex.getBindingResult()
@@ -70,13 +63,27 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(problem);
     }
 
+    @ExceptionHandler(InfrastructureException.class)
+    public ResponseEntity<ProblemDetail> handleInfrastructureException(InfrastructureException ex) {
+        log.error("Handled InfrastructureException: {}", ex.getMessage(), ex);
+
+        ProblemDetail problem = ex.getBody();
+        problem.setProperty("solution", "error.infrastructure.solution");
+
+        return ResponseEntity
+                .status(ex.getStatusCode())
+                .body(problem);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ProblemDetail> handleUnexpectedException(Exception ex) {
         log.error("Unhandled exception: {}", ex.getMessage(), ex);
 
         String title = messageResolver.getMessage("error.unexpected.title");
         String detail = messageResolver.getMessage("error.unexpected.detail");
+
         ProblemDetail problem = buildProblemDetail(HttpStatus.INTERNAL_SERVER_ERROR, title, detail);
+        problem.setProperty("solution", "error.unexpected.solution");
 
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
